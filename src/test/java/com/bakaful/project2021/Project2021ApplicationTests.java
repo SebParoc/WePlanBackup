6abcc5cb47b885903cb2ebabed50a5781a3ca550
Sebastian Paz <spazrocha@unibz.it>
2021-05-08 23:42:21 -0500

    Changed dependencies... again

---
+++ src/test/java/com/bakaful/project2021/Project2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakaful.project2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2021ApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
