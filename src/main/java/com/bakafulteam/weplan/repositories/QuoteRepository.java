34c8d10133aa0d8e4cd1a30aa3c62cd68b7d6778
Sebastian Paz <spazrocha@unibz.it>
2021-06-18 05:20:49 -0500

    deleted useless files

--- src/main/java/com/bakafulteam/weplan/repositories/QuoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bakafulteam.weplan.repositories;
-
-import com.bakafulteam.weplan.domains.Quote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuoteRepository extends JpaRepository<Quote, Long>{
-}
